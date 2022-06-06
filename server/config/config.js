556821e923566327ddd21de99d35fdebb9855fee
Wilber Donayres Olayunca <wilberd29@gmail.com>
2022-06-06 17:13:21 -0500

    Google sign in por Wilber

--- server/config/config.js
+++ server/config/config.js
@@ -28,4 +28,5 @@ process.env.CADUCIDAD_TOKEN=60 * 60 * 24 * 30;
 process.env.FIRMA= process.env.FIRMA || 'secret-demo';
 
 //Google client Id
-process.env.CLIENT_ID=process.env.CLIENT_ID || "339317350182-0c86k9gallaag2p69ltsrcdl1l22fuh3.apps.googleusercontent.com";
+process.env.CLIENT_ID=process.env.CLIENT_ID || '339317350182-0c86k9gallaag2p69ltsrcdl1l22fuh3.apps.googleusercontent.com';
+
